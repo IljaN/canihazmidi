e1a7a51e6abaa2d021329092322a402a9bde3c32
Ilja Neumann <ineumann@owncloud.com>
2018-03-25 20:24:37 +0200

    Code style

--- src/prolink2midi/pkg/midi/clock.go
+++ src/prolink2midi/pkg/midi/clock.go
@@ -8,37 +8,41 @@ import (
 
 // Midi standard commands
 const (
-	StatusStart    = 0xfa
-	StatusStop     = 0xfc
-	StatusTick     = 0xf8
-	StatusContinue = 0xfb
+	Start    = 0xFA
+	Stop     = 0xFC
+	Tick     = 0xF8
+	Continue = 0xFB
 )
 
+// Pulses per quarter note
+const ppqn = 24
+const uSecInMin = 6000000
+
 type Clock struct {
-	cmd       chan []byte
+	midiOut   chan []byte
 	pulseRate chan time.Duration
-	dev       *os.File
+	device    *os.File
 	Playing   bool
 }
 
 // Create a new midi clock. The clock starts to send tick events as soon as it is created.
-//dev, err := os.OpenFile("/dev/snd/midiC1D0", os.O_WRONLY, 0664)
-//if err != nil {
+// device, err := os.OpenFile("/device/snd/midiC1D0", os.O_WRONLY, 0664)
+// if err != nil {
 //	log.Fatal(err)
-//}
+// }
 //
-//clk := midi.NewClock(dev)
-//clk.SetBpm(120.00)
-//clk.Start()
-func NewClock(midiDevice *os.File) *Clock {
-	clk := new(Clock)
-	clk.dev = midiDevice
-	clk.cmd = make(chan []byte)
-	clk.pulseRate = make(chan time.Duration)
-	clk.Playing = false
+// clk := midi.NewClock(device)
+// clk.SetBpm(120.00)
+// clk.Start()
+func NewClock(device *os.File) *Clock {
+	clk := &Clock{
+		device:    device,
+		midiOut:   make(chan []byte),
+		pulseRate: make(chan time.Duration),
+		Playing:   false,
+	}
 
 	go clk.run()
-	clk.Stop()
 
 	return clk
 }
@@ -50,20 +54,20 @@ func (clk *Clock) SetBpm(bpm float32) {
 
 // Send MIDI sequencer start event
 func (clk *Clock) Start() {
+	clk.midiOut <- []byte{Start}
 	clk.Playing = true
-	clk.cmd <- []byte{StatusStart}
 }
 
 // Send MIDI sequencer stop event
 func (clk *Clock) Stop() {
+	clk.midiOut <- []byte{Stop}
 	clk.Playing = false
-	clk.cmd <- []byte{StatusStop}
 }
-
 
 // Send MIDI sequencer stop event
 func (clk *Clock) Continue() {
-	clk.cmd <- []byte{StatusContinue}
+	clk.midiOut <- []byte{Continue}
+	clk.Playing = true
 }
 
 func (clk *Clock) run() {
@@ -73,7 +77,7 @@ func (clk *Clock) run() {
 
 	go func() {
 		for range t.C {
-			clk.dev.Write([]byte{StatusTick})
+			clk.device.Write([]byte{Tick})
 		}
 	}()
 
@@ -81,18 +85,14 @@ func (clk *Clock) run() {
 		select {
 		case newPulseRate := <-clk.pulseRate:
 			t.SetDuration(newPulseRate)
-		case cmd := <-clk.cmd:
-			clk.dev.Write(cmd)
+		case cmd := <-clk.midiOut:
+			clk.device.Write(cmd)
 		}
 	}
 
 }
 
-// Pulses per quarter note
-const ppqn = 24
-const usec_in_min = 6000000
-
 // Converts bpm to a 24ppqn pulse interval in microseconds
 func bpmToPulseInterval(bpm float32) time.Duration {
-	return time.Duration((usec_in_min/(bpm/10))/ppqn) * time.Microsecond
+	return time.Duration((uSecInMin/(bpm/10))/ppqn) * time.Microsecond
 }
