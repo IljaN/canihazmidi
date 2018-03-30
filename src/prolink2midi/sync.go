4120bac0a92193277ef81e5d0dc90cbe58fd651e
Ilja Neumann <ineumann@owncloud.com>
2018-03-30 03:32:24 +0200

    Sync tempo on player pause

--- src/prolink2midi/sync.go
+++ src/prolink2midi/sync.go
@@ -2,37 +2,51 @@ package main
 
 import (
 	"canihazmidi/src/prolink2midi/midi"
-	"fmt"
 	"go.evanpurkhiser.com/prolink"
 )
 
-type sync struct {
-	mon *prolink.CDJStatusMonitor
-	clk *midi.Clock
+type Sync struct {
+	mon               *prolink.CDJStatusMonitor
+	clk               *midi.Clock
+	currentBPM        float32
+	lastBeatInMeasure uint8
 }
 
-func newSynchronizer(mon *prolink.CDJStatusMonitor, clk *midi.Clock) *sync {
-	s := &sync{mon: mon, clk: clk}
+func newSynchronizer(mon *prolink.CDJStatusMonitor, clk *midi.Clock) *Sync {
+	s := &Sync{mon: mon, clk: clk}
 	s.clk.Stop()
 	s.mon.OnStatusUpdate(prolink.StatusHandlerFunc(s.sync))
 
 	return s
 }
 
-func (s *sync) sync(status *prolink.CDJStatus) {
-	fmt.Println(status.String())
+func (s *Sync) setBpm(bpm float32) {
+	if s.currentBPM != bpm {
+		s.clk.SetBpm(bpm)
+		s.currentBPM = bpm
+	}
+}
+
+func (s *Sync) sync(status *prolink.CDJStatus) {
 	if !status.IsMaster {
 		return
 	}
 
-	//s.clk.SetBpm(status.TrackBPM)
+	if status.PlayState == prolink.PlayStatePaused {
+		if s.clk.Playing {
+			s.clk.Stop()
+		}
+		s.setBpm(calcEffectiveBpm(status.TrackBPM, status.SliderPitch))
 
-	if status.PlayState == prolink.PlayStatePlaying && status.BeatInMeasure == 1 && !s.clk.Playing {
-		s.clk.Start()
 		return
 	}
 
-	if status.PlayState == prolink.PlayStatePaused {
-		s.clk.Stop()
+	if !s.clk.Playing && status.PlayState == prolink.PlayStatePlaying && status.BeatInMeasure == 1 && status.Beat > 0 {
+		s.clk.Start()
 	}
+
+}
+
+func calcEffectiveBpm(bpm float32, pitch float32) float32 {
+	return bpm + ((bpm / 100.000) * pitch)
 }
